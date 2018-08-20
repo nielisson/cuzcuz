7bafe7426fa531657424e67a15831df6ae01e3d8
Unknown <cyrus135@hotmail.fr>
2018-08-20 15:11:37 +0800

    Added matchmaking mechanics

---
+++ Assets/CloudMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CloudMovement : MonoBehaviour {
+
+	private int speed;
+	private Vector3 translation;
+
+	void Start(){
+		speed = Random.Range(1, 3);
+		translation = new Vector3(speed, 0, 0);
+	}
+
+	void Update () {
+		gameObject.transform.Translate(translation);
+	}
+}
